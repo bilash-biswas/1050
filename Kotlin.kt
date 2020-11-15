bf435cd1e5e4938a9fe778f36c54b969e69a094d
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-15 17:21:57 +0600

    Create  Kotlin.kt

---
+++ Kotlin.kt
@@ -0,0 +1,19 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val read = Scanner(System.`in`);
+    var a = read.nextInt()
+    when(a){
+        61 -> println("Brasilia");
+        71 -> println("Salvador");
+        11 -> println("Sao Paulo");
+        21 -> println("Rio de Janeiro");
+        32 -> println("Juiz de Fora");
+        19 -> println("Campinas");
+        27 -> println("Vitoria");
+        31 -> println("Belo Horizonte");
+        else ->println("DDD nao cadastrado");
+    }
+}
+
+    
