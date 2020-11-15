2158c0db5383ee7b4ec1bac6aaf54c48a7b4eb9f
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-15 17:31:46 +0600

    Create  Ruby.rb

---
+++ Ruby.rb
@@ -0,0 +1,21 @@
+a = gets().chomp().to_i
+case a
+when 61
+  printf("Brasilia\n");
+when 71
+  printf("Salvador\n");
+when 11
+  printf("Sao Paulo\n");
+when 21
+  printf("Rio de Janeiro\n");
+when 32
+  printf("Juiz de Fora\n");
+when 19
+  printf("Campinas\n");
+when 27
+  printf("Vitoria\n");
+when 31
+  printf("Belo Horizonte\n");
+else
+  printf("DDD nao cadastrado\n");
+end
