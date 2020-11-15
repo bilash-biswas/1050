29f3ac8dc92df9e2bacda32d2ea95e50191b509e
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-15 17:30:42 +0600

    Create  Lua.lua

---
+++ Lua.lua
@@ -0,0 +1,20 @@
+a = io.read("*n")
+if a == 61 then
+    print("Brasilia")
+elseif a == 71 then
+    print("Salvador")
+elseif a == 11 then
+    print("Sao Paulo")
+elseif a == 21 then
+    print("Rio de Janeiro")
+elseif a == 32 then
+    print("Juiz de Fora")
+elseif a == 19 then
+    print("Campinas")
+elseif a == 27 then
+    print("Vitoria")
+elseif a == 31 then
+    print("Belo Horizonte")
+else
+    print("DDD nao cadastrado")
+end
